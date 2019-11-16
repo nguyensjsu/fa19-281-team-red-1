8edb4a6c27b78ff814d49530862cd34023afa71e
Yuxiang Chen <chenyuxiang8160@gmail.com>
2019-11-16 15:28:16 -0800

    user auth

--- user-authentication.go
+++ user-authentication.go
@@ -1,14 +1,34 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 
+	"github.com/globalsign/mgo/bson"
+
+	"github.com/globalsign/mgo"
 	"github.com/gorilla/mux"
+	"golang.org/x/crypto/bcrypt"
 )
 
+type userinfo struct {
+	Username string `json:"Username"`
+	Password string `json:"Password"`
+}
+
+type userinfoDB struct {
+	Username string `json:"Username"`
+	Password []byte `json:"Password"`
+}
+
+var c *mgo.Collection
+
 func main() {
+	c = initDB()
+
 	router := mux.NewRouter().StrictSlash(true)
 
 	router.HandleFunc("/", root).Methods("Get")
@@ -20,14 +40,78 @@ func main() {
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
+func initDB() *mgo.Collection {
+	url := "localhost:27017"
+	database := "user_auth"
+	collection := "userinfo"
+
+	session, err := mgo.Dial(url)
+	if err != nil {
+		log.Println("[initDB] " + err.Error())
+	}
+
+	return session.DB(database).C(collection)
+}
+
 func signup(w http.ResponseWriter, req *http.Request) {
 	log.Println("In signup")
-	fmt.Fprintf(w, "In Signup")
+
+	reqBody, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "Signup Error: "+err.Error())
+		return
+	}
+
+	var user userinfo
+	json.Unmarshal(reqBody, &user)
+	log.Println("Username: " + user.Username + " Password: " + user.Password)
+
+	if len(user.Username) == 0 || len(user.Password) == 0 {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "Signup Error")
+	}
+
+	hashedPassword, _ := bcrypt.GenerateFromPassword([]byte(user.Password), 8)
+	userDB := userinfoDB{user.Username, hashedPassword}
+
+	c.Insert(&userDB)
+
+	w.WriteHeader(http.StatusCreated)
+
+	fmt.Fprintf(w, "User Created")
 }
 
 func signin(w http.ResponseWriter, req *http.Request) {
 	log.Println("In signin")
-	fmt.Fprintf(w, "In Signin")
+
+	reqBody, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "Signup Error: "+err.Error())
+		return
+	}
+
+	var user userinfo
+	json.Unmarshal(reqBody, &user)
+	log.Println("Username: " + user.Username + " Password: " + user.Password)
+
+	var result userinfoDB
+	err = c.Find(bson.M{"username": user.Username}).One(&result)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "Signup Error: "+err.Error())
+		return
+	}
+
+	log.Println("Username: " + result.Username + " Password: " + string(result.Password))
+
+	if err = bcrypt.CompareHashAndPassword([]byte(result.Password), []byte(user.Password)); err != nil {
+		// If the two passwords don't match, return a 401 status
+		w.WriteHeader(http.StatusUnauthorized)
+	}
+
+	fmt.Fprintf(w, "[In Signin] Username: "+result.Username)
 }
 
 func root(w http.ResponseWriter, req *http.Request) {
