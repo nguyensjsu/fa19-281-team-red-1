7f43ff38f0970c12031fa4bc1052899b0804e2f6
Kevin Chuang <kevinchuang7@gmail.com>
2019-11-16 17:38:10 -0800

    Added history feature in user table, handled POST request from URL shortener, and GET request for getting history

--- user-authentication/user-authentication.go
+++ user-authentication/user-authentication.go
@@ -7,37 +7,60 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/globalsign/mgo/bson"
-
+	"github.com/codegangsta/negroni"
 	"github.com/globalsign/mgo"
+	"github.com/globalsign/mgo/bson"
 	"github.com/gorilla/mux"
+	"github.com/unrolled/render"
 	"golang.org/x/crypto/bcrypt"
 )
 
 type userinfo struct {
-	Username string `json:"Username"`
-	Password string `json:"Password"`
+	Username string `json:"Username" bson:"Username"`
+	Password string `json:"Password" bson:"Password"`
 }
 
 type userinfoDB struct {
-	Username string `json:"Username"`
-	Password []byte `json:"Password"`
+	Username string   `json:"Username" bson:"Username"`
+	Password []byte   `json:"Password" bson:"Password"`
+	History  []string `json:"History" bson:"History"`
+}
+
+// type userinfohistory struct {
+// 	Id       bson.ObjectId `json:"_id" bson:"_id"`
+// 	Username string        `json:"Url" bson:"Url"`
+// 	Password string        `json:"Password" bson:"Password"`
+// 	History  []string      `json:"History" bson:"History"`
+// }
+
+type requestStruct struct {
+	Url      string
+	Username string
 }
 
 var c *mgo.Collection
+var formatter *render.Render
 
 func main() {
-	c = initDB()
+	formatter = render.New(render.Options{
+		IndentJSON: true,
+	})
 
+	c = initDB()
+	n := negroni.Classic()
 	router := mux.NewRouter().StrictSlash(true)
 
-	router.HandleFunc("/", root).Methods("Get")
-	router.HandleFunc("/hello", hello).Methods("Get")
-	router.HandleFunc("/headers", headers).Methods("Get")
+	router.HandleFunc("/", root).Methods("GET")
+	router.HandleFunc("/hello", hello).Methods("GET")
+	router.HandleFunc("/headers", headers).Methods("GET")
 	router.HandleFunc("/signup", signup).Methods("POST")
 	router.HandleFunc("/signin", signin).Methods("POST")
-
-	log.Fatal(http.ListenAndServe(":8080", router))
+	router.HandleFunc("/url", urlHandler).Methods("POST")
+	router.HandleFunc("/history", historyHandler).Methods("GET")
+	n.UseHandler(router)
+	// http.ListenAndServe(":8080", n)
+	n.Run(":8080")
+	// log.Fatal(http.ListenAndServe(":8080", router))
 }
 
 func initDB() *mgo.Collection {
@@ -49,8 +72,58 @@ func initDB() *mgo.Collection {
 	if err != nil {
 		log.Println("[initDB] " + err.Error())
 	}
-
+	session.SetMode(mgo.Monotonic, true)
 	return session.DB(database).C(collection)
+}
+
+// Create history for a user
+func urlHandler(w http.ResponseWriter, req *http.Request) {
+	var request requestStruct
+	_ = json.NewDecoder(req.Body).Decode(&request)
+	fmt.Println("URL in request: ", request.Url)
+	fmt.Println("Username in request: ", request.Username)
+	query := bson.M{"Username": request.Username}
+	var user userinfoDB
+	err := c.Find(query).One(&user)
+	if err != nil {
+		formatter.JSON(w, http.StatusNotFound, "User not found")
+		// w.WriteHeader(http.StatusNotFound)
+		// fmt.Fprintf(w, "User not found")
+		return
+		// log.Fatal(err)
+	}
+	fmt.Println(user.Username)
+	// fmt.Println(user.History)
+	user.History = append(user.History, request.Url)
+	fmt.Println(user.History)
+	change := bson.M{"$set": bson.M{"History": user.History}}
+	err = c.Update(query, change)
+	if err != nil {
+		formatter.JSON(w, http.StatusInternalServerError, "Could not update database")
+		// w.WriteHeader(http.StatusInternalServerError)
+		// fmt.Fprintf(w, "Could not update database")
+		return
+	}
+	formatter.JSON(w, http.StatusCreated, "Successfully added to history")
+	// w.WriteHeader(http.StatusCreated)
+	// fmt.Fprintf(w, "Successfully added to history")
+
+}
+
+func historyHandler(w http.ResponseWriter, req *http.Request) {
+	result := []userinfoDB{}
+	err := c.Find(nil).All(&result)
+	if err != nil {
+		fmt.Println(err) // prints 'document is nil'
+	}
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	fmt.Println("Results All: ", result)
+	// w.WriteHeader(http.StatusOK)
+	// fmt.Fprintf(w, result)
+	// fmt.Println("Gumball Machine:", result)
+	formatter.JSON(w, http.StatusOK, result)
 }
 
 func signup(w http.ResponseWriter, req *http.Request) {
@@ -58,8 +131,9 @@ func signup(w http.ResponseWriter, req *http.Request) {
 
 	reqBody, err := ioutil.ReadAll(req.Body)
 	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		fmt.Fprintf(w, "Signup Error: "+err.Error())
+		formatter.JSON(w, http.StatusBadRequest, "Signup Error: "+err.Error())
+		// w.WriteHeader(http.StatusBadRequest)
+		// fmt.Fprintf(w, "Signup Error: "+err.Error())
 		return
 	}
 
@@ -68,18 +142,19 @@ func signup(w http.ResponseWriter, req *http.Request) {
 	log.Println("Username: " + user.Username + " Password: " + user.Password)
 
 	if len(user.Username) == 0 || len(user.Password) == 0 {
-		w.WriteHeader(http.StatusBadRequest)
-		fmt.Fprintf(w, "Signup Error")
+		formatter.JSON(w, http.StatusBadRequest, "Signup Error: "+err.Error())
+		// w.WriteHeader(http.StatusBadRequest)
+
+		// fmt.Fprintf(w, "Signup Error")
 	}
 
 	hashedPassword, _ := bcrypt.GenerateFromPassword([]byte(user.Password), 8)
-	userDB := userinfoDB{user.Username, hashedPassword}
-
+	userDB := userinfoDB{user.Username, hashedPassword, make([]string, 0)}
 	c.Insert(&userDB)
+	formatter.JSON(w, http.StatusCreated, "User Created")
 
-	w.WriteHeader(http.StatusCreated)
-
-	fmt.Fprintf(w, "User Created")
+	// w.WriteHeader(http.StatusCreated)
+	// fmt.Fprintf(w, "User Created")
 }
 
 func signin(w http.ResponseWriter, req *http.Request) {
@@ -87,8 +162,9 @@ func signin(w http.ResponseWriter, req *http.Request) {
 
 	reqBody, err := ioutil.ReadAll(req.Body)
 	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		fmt.Fprintf(w, "Signup Error: "+err.Error())
+		formatter.JSON(w, http.StatusBadRequest, "Signin Error: "+err.Error())
+		// w.WriteHeader(http.StatusBadRequest)
+		// fmt.Fprintf(w, "Signup Error: "+err.Error())
 		return
 	}
 
@@ -99,8 +175,9 @@ func signin(w http.ResponseWriter, req *http.Request) {
 	var result userinfoDB
 	err = c.Find(bson.M{"username": user.Username}).One(&result)
 	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		fmt.Fprintf(w, "Signup Error: "+err.Error())
+		formatter.JSON(w, http.StatusBadRequest, "Signin Error: "+err.Error())
+		// w.WriteHeader(http.StatusBadRequest)
+		// fmt.Fprintf(w, "Signup Error: "+err.Error())
 		return
 	}
 
@@ -108,7 +185,8 @@ func signin(w http.ResponseWriter, req *http.Request) {
 
 	if err = bcrypt.CompareHashAndPassword([]byte(result.Password), []byte(user.Password)); err != nil {
 		// If the two passwords don't match, return a 401 status
-		w.WriteHeader(http.StatusUnauthorized)
+		// w.WriteHeader(http.StatusUnauthorized)
+		formatter.JSON(w, http.StatusUnauthorized, "Unauthorized")
 	}
 
 	fmt.Fprintf(w, "[In Signin] Username: "+result.Username)
